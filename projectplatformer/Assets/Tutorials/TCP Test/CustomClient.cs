4af9abf72b37b974a3120e0041bebcd36486ccbb
Niek van Unen <528715@student.glu.nl>
2020-02-11 12:42:05 +0100

    Mobile controller added

---
+++ projectplatformer/Assets/Tutorials/TCP Test/CustomClient.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This Client inheritated class acts like Client using UI elements like buttons and input fields.
+/// </summary>
+public class CustomClient : Client
+{
+    [SerializeField] private Text m_ClientLogger = null;
+
+    //Set UI interactable properties
+    private void Awake()
+    {
+        base.StartClient();
+    }
+
+    private void OnDisable()
+    {
+        SendCloseToServer();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+         SendMessageToServer();
+    }
+
+    private void SendMessageToServer()
+    {
+        string newMsg = "Test message ;0 "+ Time.time;
+        base.SendMessageToServer(newMsg);
+    }
+
+    private void SendCloseToServer()
+    {
+        base.SendMessageToServer("Close");
+    }
+
+    //Custom Client Log
+    #region ClientLog
+    protected override void ClientLog(string msg, Color color)
+    {
+        base.ClientLog(msg, color);
+        //m_ClientLogger.text += '\n' + "<color=#" + ColorUtility.ToHtmlStringRGBA(color) + ">- " + msg + "</color>";
+    }
+    protected override void ClientLog(string msg)
+    {
+        base.ClientLog(msg);
+        //m_ClientLogger.text += '\n' + "- " + msg;
+    }
+    #endregion
+}
---
+++ projectplatformer/Assets/Tutorials/TCP Test/CustomServer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This Server inheritated class acts like Server using UI elements like buttons and input fields.
+/// </summary>
+public class CustomServer : Server
+{
+
+    //Set UI interactable properties
+    protected virtual void Awake()
+    {
+        StartServer();
+    }
+
+    //Start server and wait for clients
+    protected override void StartServer()
+    {
+        base.StartServer();
+    }
+
+    //Get input field text and send it to client
+
+    //Close connection with the client
+    protected override void CloseClientConnection()
+    {
+        base.CloseClientConnection();
+    }
+
+    //Close client connection and disables the server
+    protected override void CloseServer()
+    {
+        base.CloseServer();
+    }
+
+    //Custom Server Log
+    #region ServerLog
+    //With Text Color
+    protected override void ServerLog(string msg)
+    {
+        base.ServerLog(msg);
+        print(msg);
+    }
+    //Without Text Color
+    protected override void ServerLog(string msg, Color color)
+    {
+        base.ServerLog(msg, color);
+        print(msg);
+    }
+    #endregion
+}
