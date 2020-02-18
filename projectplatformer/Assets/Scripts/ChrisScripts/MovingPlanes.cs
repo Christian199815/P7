76f6303a3c766a4b98ce5eb7258c58a5685f31df
MAJOR TEMPTATION <45289692+FunkyPolarBear@users.noreply.github.com>
2020-02-18 08:56:16 +0100

    Unity BlockUp Tut Level

---
+++ projectplatformer/Assets/Scripts/ChrisScripts/MovingPlanes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlanes : MonoBehaviour
+{
+    public GameObject Plane;
+    public int MovSpeed;
+
+    
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        
+    }
+
+    void Move()
+    {
+        Plane.transform.position = new Vector3(Plane.transform.position.x - (MovSpeed * Time.smoothDeltaTime), Plane.transform.position.y, Plane.transform.position.z);
+    }
+}
