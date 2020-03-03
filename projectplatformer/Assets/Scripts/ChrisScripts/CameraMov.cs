4c3b9d5f7718e41f14dffeb642c30a4d56c20398
MAJOR TEMPTATION <45289692+FunkyPolarBear@users.noreply.github.com>
2020-03-03 17:25:37 +0100

    shizzle

--- projectplatformer/Assets/Scripts/ChrisScripts/CameraMov.cs
+++ projectplatformer/Assets/Scripts/ChrisScripts/CameraMov.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraMov : MonoBehaviour
 {
     public GameObject Player;
+    public Camera cm;
+    private bool isTopFloor = true;
+    public int camSize = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +19,16 @@ public class CameraMov : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(Player.transform.position.x, transform.position.y, transform.position.z);
+        CameraAndLift();
     }
 
-    void CameraMovement()
+    void CameraAndLift()
     {
-
+        if (transform.position.x >= 63f && isTopFloor)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, -27.5f, transform.position.z), 2f * Time.deltaTime);
+            cm.orthographicSize = 15;
+            isTopFloor = false;
+        }
     }
 }
